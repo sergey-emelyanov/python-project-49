1234dd8c38f38fae68c7c686f0c29ab2c941ee56
sergey-emelyanov <emelia35@yandex.ru>
2022-11-05 19:21:34 +0300

    add prime_game

---
+++ brain_games/games/prime_game.py
@@ -0,0 +1,21 @@
+import random
+
+description = 'Answer "yes" if given number is prime. Otherwise answer "no".'
+
+
+def data_about_round():
+	number = random.randint(1, 100)
+	if is_prime(number):
+		correct_answer = 'yes'
+	else:
+		correct_answer = 'no'
+	return number, correct_answer
+
+
+def is_prime(number):
+	flag = True
+	for i in range(2, int(number**0.5)+1):
+		if number % i == 0:
+			flag  = False
+			break
+	return flag
---
+++ brain_games/scripts/brain_prime.py
@@ -0,0 +1,9 @@
+from brain_games.games import prime_game
+from brain_games.launch import launch
+
+def main():
+	launch(prime_game)
+
+
+if __name__ == "__main__":
+	main()
