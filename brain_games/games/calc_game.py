6561c0ae8ffdd8dbbc4e92ad9b4a43364d5e55e7
sergey-emelyanov <emelia35@yandex.ru>
2022-11-06 19:58:49 +0300

    fix some bugs

--- brain_games/games/calc_game.py
+++ brain_games/games/calc_game.py
@@ -7,7 +7,7 @@ def data_about_round():
 	number1 = random.randint(1, 100)
 	number2 = random.randint(1, 100)
 	sign = random.choice(('+', '-', '*'))
-	question = str(number1) + sign + str(number2)
+	question = f"{str(number1)} {sign} {str(number2)}"
 	correct_answer = str(calculatuon(number1, number2, sign))
 	return question, correct_answer
 
